0ab57bb6c10c4cffe2969ddaf13af37ee4b85491
Charlie <cmilla11@jh.edu>
2022-09-08 23:24:24 -0400

    commented vcf analysis script

--- day2-homework/vcf_analysis.py
+++ day2-homework/vcf_analysis.py
@@ -1,47 +1,23 @@
-from vcfParser import *
-random_snippet = (parse_vcf("random_snippet.vcf"))
-dpSNP = (parse_vcf("dbSNP_snippet.vcf"))
-SNP_dict = {}
-matching =[]
+from vcfParser import * #import parse_vcf function
+random_snippet = (parse_vcf("random_snippet.vcf")) #parse random_snippet.vcf into the list random_snippet
+dpSNP = (parse_vcf("dbSNP_snippet.vcf")) #parse dbSNP_snippet.vcf into the list dbSNP
+SNP_dict = {} #initialize the dictionary SNP_dict
+matching =[] #initialize the list matching
 
-for i, line in enumerate(dpSNP):
-    if i==0:
-        continue
-    else:
-        SNP_dict[line[1]]=line[2]
-counter = 0
-for key in SNP_dict:
-    for line in random_snippet:
-        if line[1] == key:
-            line[2] = SNP_dict[key]
-            matching.append(line)
+for i, line in enumerate(dpSNP): #make variable i that equals the index of each entry in dbSNP and variable line that equals the entry
+    if i==0: #if looking at the first entry of dbSNP
+        continue # skip
+    else: #otherwise
+        SNP_dict[line[1]]=line[2] #fill SNP dict with the entries at line index 1 as keys and the entries at line index 2 as values
 
-no_matches = ((len(random_snippet[1:]))-(len(matching)))
-if no_matches > 0:
-    print(f"There were {no_matches} records that do not have corresponding IDs", file=sys.stderr)
-for i in range(len(matching)):
-    print(matching[i])
-        
-            
+for key in SNP_dict: #make variable key that matches keys in SNP_dict
+    for line in random_snippet: #make variable line that matches entries in random_snippet
+        if line[1] == key: # if the entry at line index 1 matches the key
+            line[2] = SNP_dict[key] #set the entry at line index 2 as the value of the dictionary at that key
+            matching.append(line) #append the edited line to the list matching
 
-
-#print(SNP_dict)
-
-# for line in random_snippet:
-#     if i==0:
-#         continue
-#     else:
-#         line[2]=SNP_dict["ID"]
-
-# for s in np.unique(pca["SEX"]):
-#     pca1 = []
-#     pca2 = []
-#     sex[s] = [pca1, pca2]#fill with two empty lists to be populated
-#     for lines in pca:
-#         if s == lines[3]:
-#             pca1.append(lines[5])
-#             pca2.append(lines[6])
-#     ax.scatter(sex[s][0], sex[s][1], label = s)
-#     ax.legend()
-# plt.savefig("sex_colored_pca_plot.png")
-# plt.close(fig)
+no_matches = ((len(random_snippet[1:]))-(len(matching))) #subtract the length of the matching list from the one less than the length of random_snippet (so as to avoid header) to find # entries that don't match
+if no_matches > 0: #if there are any entries in random_snippet that don't match with those in matching
+    print(f"There were {no_matches} records that do not have corresponding IDs", file=sys.stderr) #print how many entries didn't match
+for i in range(len(matching)): #make variable i that equals the index of each entry in matching
+    print(matching[i]) #print the value of matching at index i
