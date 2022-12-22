27ff815d059dbe0c9d9a4458c7e44a2cff14f80e
Charlie <cmilla11@jh.edu>
2022-12-22 01:22:10 -0500

    pca_plotting script

---
+++ week4-homework/gwas_data/PCA_plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+pca = np.genfromtxt("plink.eigenvec", dtype = None, encoding = None, names = ["ID_1", "ID_2", "PCA_1", "PCA_2", "PCA_3", "PCA_4", "PCA_5", "PCA_6", "PCA_7", "PCA_8", "PCA_9", "PCA_10"])
+
+fig, ax = plt.subplots() #create a figure and axes
+ax.scatter(pca["PCA_1"], pca["PCA_2"])
+ax.set_xlabel("PCA_1")
+ax.set_ylabel("PCA_2")
+plt.savefig("ex2_a.png")
+plt.close(fig)
+
+fig2, ax = plt.subplots()
+ax.scatter(pca["PCA_1"], pca["PCA_3"])
+ax.set_xlabel("PCA_1")
+ax.set_ylabel("PCA_3")
+plt.savefig("ex2_B.png")
+plt.close(fig2)
